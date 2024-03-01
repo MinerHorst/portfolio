297a729bc68972eba27a03554fd24104eb5ef00a
MinerHorst <105922569+MinerHorst@users.noreply.github.com>
2024-03-02 00:14:09 +0100

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
+import { ReactLenis } from "@studio-freight/react-lenis";
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ReactLenis root>
+      <Component {...pageProps} />
+    </ReactLenis>
+  );
 };
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import Head from "next/head";
 import Link from "next/link";
-import { easeInOut, motion as m } from "framer-motion";
+import { delay, easeInOut, motion as m } from "framer-motion";
+import Image from "next/image";
+
+import top from "../../public/top.png";
+import bottom from "../../public/bottom.png";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 export default function Home() {
   const children = ["L", "u", "i", "s", "K", "e", "ß", "l", "e", "r"];
@@ -22,9 +27,9 @@ export default function Home() {
           animate={{ y: "-100%" }}
           initial={{ y: 0 }}
           transition={{ delay: 2.6, duration: 1.5, ease: easeInOut }}
-          className="absolute left-0 top-0 z-[50] flex h-screen w-screen flex-col items-center justify-center bg-white text-black "
+          className="fixed left-0 top-0 z-[50] flex h-screen w-screen flex-col items-center justify-center bg-white text-black "
         >
-          <div className="[font-size:_clamp(2em,7vw,16em)]">
+          <m.div className="[font-size:_clamp(2em,7vw,16em)]">
             {children.map((letter, index) => (
               <m.span
                 key={index}
@@ -32,14 +37,72 @@ export default function Home() {
                 animate={{ opacity: 100 }}
                 transition={{ delay: 0.2 * index }}
               >
-                <span className={index < 4 ? "poppins" : "playfair italic"}>
+                <m.span className={index < 4 ? "poppins" : "playfair italic"}>
                   {letter}
-                </span>
+                </m.span>
               </m.span>
             ))}
-          </div>
+          </m.div>
         </m.div>
-        <div className="h-screen w-screen border"></div>
+
+        <div>
+          <m.div className="flex h-fit w-screen flex-col text-white">
+            <div className="flex h-screen w-screen flex-col">
+              <div className="montserrat flex h-[10vh] items-center justify-between px-3 md:px-10">
+                <p className="text-lg">
+                  <span className="poppins">Luis</span>
+                  <span className="playfair italic">Keßler</span>
+                </p>
+                <p className="hidden md:inline">
+                  <span>Index, </span>
+                  <span className="text-muted-foreground">Projects, </span>
+                  <span className="text-muted-foreground">About, </span>
+                  <span className="text-muted-foreground">Contact</span>
+                </p>
+                <p className="hidden underline md:inline">Send me a message.</p>
+              </div>
+              <div className="flex h-[90vh] flex-col items-center justify-center">
+                <m.div
+                  animate={{ y: ["0vh", "-19vh"], opacity: [1, 0.4] }}
+                  transition={{ delay: 4.2, duration: 1, ease: easeInOut }}
+                  className="flex w-full flex-col items-center justify-center"
+                >
+                  <Image src={top} alt="" className="max-w-[40%]"></Image>
+                </m.div>
+                <m.div
+                  animate={{ y: ["0vh", "19vh"], opacity: [1, 0.4] }}
+                  transition={{ delay: 4.2, duration: 1, ease: easeInOut }}
+                  className="flex w-full flex-col items-center justify-center"
+                >
+                  <Image src={bottom} alt="" className="max-w-[40%]"></Image>
+                </m.div>
+              </div>
+            </div>
+            <div className="h-screen bg-white"></div>
+          </m.div>
+          <m.div
+            animate={{
+              scaleX: [0, 1],
+            }}
+            transition={{ delay: 5.2, duration: 1.5 }}
+            className="absolute left-0 top-0 flex h-screen w-screen items-center justify-center pt-[10vh]"
+          >
+            <div className="flex h-[50%] w-full items-center justify-between overflow-scroll">
+              <div className="h-full w-full">
+                <div className="grid h-full w-[300vw] grid-cols-8 bg-red-500">
+                  <div className="col-span-1 border"></div>
+                  <div className="col-span-1 border"></div>
+                  <div className="col-span-1 border"></div>
+                  <div className="col-span-1 border"></div>
+                  <div className="col-span-1 border"></div>
+                  <div className="col-span-1 border"></div>
+                  <div className="col-span-1 border"></div>
+                  <div className="col-span-1 border"></div>
+                </div>
+              </div>
+            </div>
+          </m.div>
+        </div>
       </m.main>
     </>
   );
