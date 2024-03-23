bbd6afdae1d95ab1fd7030a26b218cb8c74d3a20
MinerHorst <105922569+MinerHorst@users.noreply.github.com>
2024-03-23 14:25:30 +0100

    changed folder structure

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import { ReactLenis } from "@studio-freight/react-lenis";
 import { type AppType } from "next/dist/shared/lib/utils";
 import { ParallaxProvider } from "react-scroll-parallax";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ParallaxProvider>
+      <Analytics />
+      <SpeedInsights />
       <ReactLenis root>
         <Component {...pageProps} />
       </ReactLenis>
