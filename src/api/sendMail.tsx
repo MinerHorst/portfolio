ea2f539e319f1882834c1e92e1048db9ac21fd33
MinerHorst <105922569+MinerHorst@users.noreply.github.com>
2024-03-23 15:09:43 +0100

    commit

--- src/api/sendMail.tsx
+++ src/api/sendMail.tsx
@@ -2,8 +2,8 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import mailgun from "mailgun-js";
 
 const mg = mailgun({
-  apiKey: "1dee71cfc31a2638aa36adb124578c99-309b0ef4-9646ccd2",
-  domain: "sandbox6e4aa54ba19647f6b1c891042f677411.mailgun.org",
+  apiKey: process.env.MAILGUN_API_KEY!,
+  domain: process.env.MAILGUN_DOMAIN!,
 });
 
 export default async (req: VercelRequest, res: VercelResponse) => {
